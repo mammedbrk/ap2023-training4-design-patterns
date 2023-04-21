96a9c27f9306a619ff17fd41675f7164ddd813db
mammedbrk <mammedbrk@gmail.com>
2023-04-21 12:14:05 +0330

    Modify Config class

--- src/main/java/com/mammedbrk/config/Config.java
+++ src/main/java/com/mammedbrk/config/Config.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-    public static Properties getConfig() {
+    public static final String CONFIG_ADDRESS = "src/main/resources/com.mammedbrk/config/";
+
+    public static Properties getConfig(String name) {
         Properties properties = new Properties();
         try {
-            properties.load(new FileReader("src/main/resources/com.mammedbrk/config/board.properties"));
+            properties.load(new FileReader(CONFIG_ADDRESS + name + ".properties"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
